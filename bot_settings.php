16d60f1545dca30b782dbbc77c9de9a4184a980f
menphurk <sorasak_456123@hotmail.com>
2019-03-26 16:13:46 +0700

    first work

---
+++ bot_settings.php
@@ -0,0 +1,6 @@
+<?php
+/// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
+define('LINE_MESSAGE_CHANNEL_ID','1558722901');
+define('LINE_MESSAGE_CHANNEL_SECRET','872c9e1f6b247862e42fe2185d7f76b0');
+define('LINE_MESSAGE_ACCESS_TOKEN','blUxIMxvtkOfxUSRxJfgSyck4H7+mK6twIBvLlfeDlfzNluacEl6MDBFsG8BcgTZUDsYlRwYx3C8pknxV2JwosIhLHWmlEQbTVtoadbclIAfp4lghQuw1a/x1tvAWQxQl2Tuk1HfnPqvpXO/I23AMQdB04t89/1O/w1cDnyilFU=');
+?>
